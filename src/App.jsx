674179bc57e0989c45a2e88d14d286187ec33911
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-04-28 20:28:07 +0300

    task-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import Profile from './components/Profile';
+import Profile from './components/Profile/Profile';
 import userData from './userData.json';
+
+import FriendList from './components/FriendList/FriendList';
+import friends from './friends.json';
 
 export default function App() {
   return (
@@ -11,6 +14,7 @@ export default function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 }
